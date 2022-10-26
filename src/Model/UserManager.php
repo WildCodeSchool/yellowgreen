5eeff19c020b660f7aeabc6597ff5478d32366eb
thenriet <dabalx@gmail.com>
2022-10-26 16:51:49 +0200

    deleted UserManager.php so it wont make conflicts with other merges

---
+++ src/Model/UserManager.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Model;
+
+use PDO;
+
+class UserManager extends AbstractManager
+{
+    public const TABLE = 'user';
+
+    /**
+     * Insert new user in database
+     */
+    public function insert(array $user): int
+    {
+        $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE . " (`firstname`)");
+        $statement->execute();
+        return (int)$this->pdo->lastInsertId();
+    }
+
+    /**
+     * Update user in database
+     */
+    public function update(array $user): bool
+    {
+        $statement = $this->pdo->prepare("UPDATE " . self::TABLE . " SET `title` = :title WHERE id=:id");
+        $statement->bindValue('id', $user['id'], PDO::PARAM_INT);
+        $statement->bindValue('title', $user['title'], PDO::PARAM_STR);
+
+        return $statement->execute();
+    }
+}
