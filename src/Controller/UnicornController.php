5c254716e3a34ddcf564259a8f2c7dd1cd6ab34d
thenriet <dabalx@gmail.com>
2022-11-06 21:12:25 +0100

    manager, controller and view created for unicorn

---
+++ src/Controller/UnicornController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use App\Model\UnicornManager;
+
+class UnicornController extends AbstractController
+{
+    /**
+     * List users
+     */
+    public function index(): string
+    {
+        $unicornManager = new UnicornManager();
+        $unicorns = $unicornManager->selectAll("score", "desc");
+
+        return $this->twig->render('Unicorn/index.html.twig', ['unicorns' => $unicorns]);
+    }
+    /**
+     * Show informations for a specific unicorn
+     */
+    public function show(int $id): string
+    {
+        $unicornManager = new UnicornManager();
+        $unicorn = $unicornManager->selectOneById($id);
+        return $this->twig->render('Unicorn/show.html.twig', ['unicorn' => $unicorn]);
+    }
+
+}
