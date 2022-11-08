be985390f5a4eeec0206d59b0fe0edcd61df4cf9
thenriet <dabalx@gmail.com>
2022-11-08 10:45:26 +0100

    commit before pulling dev

--- public/assets/js/select-unicorn.js
+++ public/assets/js/select-unicorn.js
@@ -1,3 +1,5 @@
+// Select and update the chosen unicorn by the user
+
 const changeSelected1 = () => {
     let select = document.getElementById('select-user');
     select.value = 1;
@@ -11,16 +13,6 @@ const changeSelected2 = () => {
 const changeSelected3 = () => {
     let select = document.getElementById('select-user');
     select.value = 3;
-};
-
-const SelectedByComputer = () => {
-    let selectOpponent = document.getElementById('select-opponent');
-    if (Math.random() * 10 < 3.3) {
-        selectOpponent.value = 1;
-    } else if (Math.random() * 10 > 3.3 && Math.random() * 10 < 6.7) {
-        selectOpponent.value = 2;
-    } else { selectOpponent.value = 3; }
-    console.log(selectOpponent.value);
 };
 
 const allena = document.getElementById('allena');
@@ -47,5 +39,33 @@ suki.addEventListener('click', () => {
     allena.classList.toggle('opacity-low');
     larissa.classList.toggle('opacity-low');
 });
+// Selection by the opponent 
+
+const SelectedByComputer = () => {
+    let selectOpponent = document.getElementById('select-opponent');
+    if (Math.random() * 10 < 3.3) {
+        selectOpponent.value = 1;
+    } else if (Math.random() * 10 > 3.3 && Math.random() * 10 < 6.7) {
+        selectOpponent.value = 2;
+    } else { selectOpponent.value = 3; }
+};
+
 
 SelectedByComputer();
+
+// Hover on the unicorn cards 
+
+let unicorns = document.querySelectorAll('.unicorn');
+
+grow = (e) => {
+    e.target.classList.toggle('normal')
+    e.target.classList.toggle('grow')
+}
+
+function eventHandler() {
+    unicorns.forEach(item => {
+        item.addEventListener("mouseenter", grow, false);
+        item.addEventListener("mouseleave", grow, false);
+    })
+}
+eventHandler();
