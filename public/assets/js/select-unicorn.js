a77820de978c73a8bbe7204dcc6a2c204f957c7c
thenriet <dabalx@gmail.com>
2022-11-09 12:03:27 +0100

    dynamic js functions for selecting unicorns

--- public/assets/js/select-unicorn.js
+++ public/assets/js/select-unicorn.js
@@ -1,55 +1,27 @@
 // Select and update the chosen unicorn by the user
-let unicorns = document.querySelectorAll('.unicorn');
-console.log(unicorns);
-
+const unicorns = document.querySelectorAll('.unicorn');
+let select = document.getElementById('userUnicorn');
 
 unicorns.forEach(unicorn => {
-    
+    unicorn.addEventListener('click', () => {
+        changeSelected(unicorn.id);
+        unicorn.classList.toggle("shadow-drop-2-center");
+        unicorns.forEach(unicorn => {
+            if (select.value != unicorn.id) {
+                unicorn.classList.toggle('opacity-low');
+            }
+        });
+    });
 });
 
-const changeSelected1 = () => {
-    let select = document.getElementById('select-user');
-    select.value = 1;
+function changeSelected(unicornId) {
+    select.value = unicornId;
 };
 
-const changeSelected2 = () => {
-    let select = document.getElementById('select-user');
-    select.value = 2;
-};
-
-const changeSelected3 = () => {
-    let select = document.getElementById('select-user');
-    select.value = 3;
-};
-
-const allena = document.getElementById('allena');
-allena.addEventListener('click', () => {
-    changeSelected1();
-    allena.classList.toggle("shadow-drop-2-center");
-    larissa.classList.toggle('opacity-low');
-    suki.classList.toggle('opacity-low');
-});
-
-
-const larissa = document.getElementById('larissa');
-larissa.addEventListener('click', () => {
-    changeSelected2();
-    larissa.classList.toggle("shadow-drop-2-center");
-    allena.classList.toggle('opacity-low');
-    suki.classList.toggle('opacity-low');
-});
-
-const suki = document.getElementById('suki');
-suki.addEventListener('click', () => {
-    changeSelected3();
-    suki.classList.toggle("shadow-drop-2-center");
-    allena.classList.toggle('opacity-low');
-    larissa.classList.toggle('opacity-low');
-});
 // Selection by the opponent 
 
 const SelectedByOpponent = () => {
-    let selectOpponent = document.getElementById('select-opponent');
+    let selectOpponent = document.getElementById('opponentUnicorn');
     if (Math.random() * 10 < 3.3) {
         selectOpponent.value = 1;
     } else if (Math.random() * 10 > 3.3 && Math.random() * 10 < 6.7) {
